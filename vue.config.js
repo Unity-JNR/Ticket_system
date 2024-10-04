7e2a725a77780d1ce68dc30fe975a8f9325b01ea
Unity-JNR <unitydemas12@gmail.com>
2024-10-04 09:57:04 +0200

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
-const fs = require('fs')
-const path = require('path');
-const admins = {
-  admin1: 'Intern-38',
-  admin2: 'Intern-58',
-  admin3: 'admin3',
-}
-module.exports = {
-  devServer: {
-    host: '192.168.8.215',
-    port: 8080,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, `../../../${admins.admin2}/ssl/local.key`)),
-      cert: fs.readFileSync(path.resolve(__dirname, `../../../${admins.admin2}/ssl/local.crt`)),
-    },
-  },
-}
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// const fs = require('fs')
+// const path = require('path');
+// const admins = {
+//   admin1: 'Intern-38',
+//   admin2: 'Intern-58',
+//   admin3: 'admin3',
+// }
+// module.exports = {
+//   devServer: {
+//     host: '192.168.8.215',
+//     port: 8080,
+//     https: {
+//       key: fs.readFileSync(path.resolve(__dirname, `../../../${admins.admin2}/ssl/local.key`)),
+//       cert: fs.readFileSync(path.resolve(__dirname, `../../../${admins.admin2}/ssl/local.crt`)),
+//     },
+//   },
+// }
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
